bb6b722b18abb0160a630757a676bfc79237a4a1
Daniel Deimling <daniel.deimling@mailbox.org>
2021-04-15 10:21:33 +0200

    changes...

---
+++ VirtualBox/chroot.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+rootPassword="root"
+userName="daniel"
+userPassword="daniel"
+
+# Set hostname
+echo Arch-Desktop-VM > /etc/hostname
+
+# Configure console
+echo KEYMAP=de-latin1 > /etc/vconsole.conf
+echo FONT=lat9w-16 >> /etc/vconsole.conf
+
+# Configure locale
+echo LANG=de_DE.UTF-8 > /etc/locale.conf
+echo LANGUAGE=de_DE >> /etc/locale.conf
+sed -i "s|#de_DE.UTF-8|de_DE.UTF-8|" /etc/locale.gen
+sed -i "s|#en_US.UTF-8|en_US.UTF-8|" /etc/locale.gen
+locale-gen
+
+# Set the timezone
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+# Install needed tools, software & services
+pacman --noconfirm --needed -S virtualbox-guest-utils sudo grub efibootmgr nano vim dhcpcd bash-completion acpid avahi cups networkmanager cronie xorg-server xorg-xinit gnome gnome-tweaks gdm firefox terminator
+
+# Enable 'sudo' for group 'wheel'
+pacman --noconfirm --needed -S sudo
+sed -i 's|# %wheel ALL=(ALL) ALL|%wheel ALL=(ALL) ALL|' /etc/sudoers
+
+# Enable services
+systemctl enable acpid
+systemctl enable avahi-daemon
+systemctl enable org.cups.cupsd
+systemctl enable NetworkManager
+systemctl enable cronie
+systemctl enable gdm
+
+# Add user
+useradd -m -g users -G wheel,audio,video,games,power -s /bin/bash $userName
+echo -e "${userPassword}\n${userPassword}" | passwd $userName
+
+# Set root passwd
+echo -e "${rootPassword}\n${rootPassword}" | passwd root
+
+# Generate initramfs
+mkinitcpio -p linux
+
+# Setup GRUB
+pacman --noconfirm --needed -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
--- VirtualBox/install.sh
+++ VirtualBox/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##Arch base system setup
+##Arch setup
 
 # Create partitions
 sgdisk /dev/sda --zap-all
@@ -22,65 +22,15 @@ cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector --verbose --sort rate --country AT,BE,DK,FI,FR,DE,GR,IE,IT,LU,NL,PT,ES,SE,GB --save /etc/pacman.d/mirrorlist
 
 # Install base system
-pacstrap /mnt base base-devel linux linux-firmware grub efibootmgr nano dhcpcd bash-completion virtualbox-guest-utils
+pacstrap /mnt base base-devel linux linux-firmware
 
 # Generate fstab with IDs
 genfstab -Up /mnt > /mnt/etc/fstab
 
-# Configure locale
-sed -i "s|#de_DE.UTF-8|de_DE.UTF-8|" /mnt/etc/locale.gen
-sed -i "s|#en_US.UTF-8|en_US.UTF-8|" /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
+curl https://raw.githubusercontent.com/ddeimling/arch-install/master/VirtualBox/chroot.sh | bash
 
-# Set the timezone
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-
-# Set 'sudo'
-arch-chroot /mnt pacman --noconfirm --needed -S sudo
-sed -i 's|# %wheel ALL=(ALL) ALL|%wheel ALL=(ALL) ALL|' /mnt/etc/sudoers
-
-# Useful services
-arch-chroot /mnt pacman --noconfirm --needed -S acpid
-arch-chroot /mnt systemctl enable acpid
-
-arch-chroot /mnt pacman --noconfirm --needed -S avahi
-arch-chroot /mnt systemctl enable avahi-daemon
-
-arch-chroot /mnt pacman --noconfirm --needed -S cups
-arch-chroot /mnt systemctl enable org.cups.cupsd
-
-arch-chroot /mnt pacman --noconfirm --needed -S networkmanager
-arch-chroot /mnt systemctl enable NetworkManager
-
-arch-chroot /mnt pacman --noconfirm --needed -S cronie
-arch-chroot /mnt systemctl enable cronie
-
-# Xorg, Desktop & Login
-arch-chroot /mnt pacman --noconfirm --needed -S xorg-server xorg-xinit gnome gdm
-arch-chroot /mnt systemctl enable gdm
-
-# Generate initramfs
-arch-chroot /mnt mkinitcpio -p linux
-
-# Setup GRUB
-arch-chroot /mnt pacman --noconfirm --needed -S grub efibootmgr
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Grub
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-
-# Add user
-arch-chroot /mnt useradd -m -g users -s /bin/bash daniel
-arch-chroot /mnt gpasswd -a daniel wheel
-arch-chroot /mnt gpasswd -a daniel audio
-arch-chroot /mnt gpasswd -a daniel video
-arch-chroot /mnt gpasswd -a daniel games
-arch-chroot /mnt gpasswd -a daniel power
-arch-chroot /mnt echo -e "daniel\ndaniel" | passwd daniel
-
-# Change root passwd
-arch-chroot /mnt echo -e "root\nroot" | passwd root
-
-## Cleanup & have fun :D
-# swapoff /dev/sda2
-# umount /dev/sda1
-# umount /dev/sda3
-# reboot
+# Cleanup & have fun
+swapoff /dev/sda2
+umount /dev/sda1
+umount /dev/sda3
+reboot
